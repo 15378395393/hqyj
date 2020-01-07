cb4c06a4377fffc2acb6969da5ce6ae6c1fc8919
Administrator <Administrator@PC-20190611GAKM>
2020-01-07 16:55:17 +0800

    "test"

---
+++ sb_1910_01/src/main/java/com/hqyj/consumer/Sb191001Application.java
@@ -0,0 +1,13 @@
+package com.hqyj.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb191001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb191001Application.class, args);
+	}
+
+}
---
+++ sb_1910_01/src/main/java/com/hqyj/consumer/modules/test/controller/TestController.java
@@ -0,0 +1,52 @@
+package com.hqyj.consumer.modules.test.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hqyj.consumer.modules.test.vo.ConfigTestBean;
+@Controller
+public class TestController extends SpringBootServletInitializer{
+	@Value("${server.port}")
+	private int port;
+	@Value("${com.hqyj.name}")
+	private String name;
+	@Value("${com.hqyj.age}")
+	private int age;
+	@Value("${com.hqyj.description}")
+	private String description;
+	@Value("${com.hqyj.random}")
+	private String random;
+	
+	@Autowired
+	private ConfigTestBean configTestBean;
+	
+	@RequestMapping("/test/config")
+	@ResponseBody
+	public String configTest() {
+		StringBuffer sb = new StringBuffer();
+		
+		sb.append(port).append("----").append("<br>")
+			.append(name).append("----").append("<br>")
+			.append(age).append("----").append("<br>")
+			.append(description).append("----").append("<br>")
+			.append(random).append("----").append("<br>");
+		
+		sb.append(configTestBean.getName()).append("----").append("<br>")
+		.append(configTestBean.getAge()).append("----").append("<br>")
+		.append(configTestBean.getDescription()).append("----").append("<br>")
+		.append(configTestBean.getRandom()).append("----").append("<br>");
+		
+		return sb.toString();
+	}
+
+	
+	@RequestMapping("/test/info")
+	@ResponseBody
+	public String appInfo() {
+		return "This is spring boot app.";
+	}
+}
